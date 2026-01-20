0cc53afd1b353e03849c5c6d68ff601bf020cd69
kayler <pavelkayler@icloud.com>
2026-01-20 18:02:23 +0300

    Add basename to browser router configuration

--- src/core/routes/Routes.jsx
+++ src/core/routes/Routes.jsx
@@ -5,6 +5,6 @@ import PortfolioPage from "../../ui/pages/portfolioPage/PortfolioPage.jsx";
 const Routes = createBrowserRouter([
   { path: "/", element: <MainPage /> },
   { path: "/portfolio", element: <PortfolioPage /> },
-]);
+],{ basename: import.meta.env.BASE_URL });
 
 export default Routes;
